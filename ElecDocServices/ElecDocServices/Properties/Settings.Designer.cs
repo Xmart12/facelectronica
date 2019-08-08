1c82ef322c2cf60faa0f52d215ca919bc1ca7e57
Mario Espinoza <mario.espinoza@dada-dada.com>
2019-08-08 17:39:54 -0600

    Addicion de nueva conexion de WebService Eforcon

--- ElecDocServices/ElecDocServices/Properties/Settings.Designer.cs
+++ ElecDocServices/ElecDocServices/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace ElecDocServices.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -40,6 +40,16 @@ namespace ElecDocServices.Properties {
         public string ElecDocServices_IngfaceServices_ingface {
             get {
                 return ((string)(this["ElecDocServices_IngfaceServices_ingface"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.WebServiceUrl)]
+        [global::System.Configuration.DefaultSettingValueAttribute("https://facturar.eforcon.com/eforconservice/Service1.asmx")]
+        public string ElecDocServices_EforconServices_Service1 {
+            get {
+                return ((string)(this["ElecDocServices_EforconServices_Service1"]));
             }
         }
     }
---
+++ ElecDocServices/ElecDocServices/Providers/Eforcon.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using ElecDocServices.Helpers;
+using ElecDocServices.EforconServices;
+
+namespace ElecDocServices.Providers
+{
+    class Eforcon : IFacElecInterface
+    {
+        private Utils utl = new Utils();
+
+        public DataTable DocHeader { get; set; }
+        public DataTable DocDetail { get; set; }
+        public DataTable DocProvider { get; set; }
+
+
+        public List<Parameter> RegistrarDocumento()
+        {
+            SSO_clsResponseGeneral res = new SSO_clsResponseGeneral();
+            string xml = null;
+
+            try
+            {
+                Service1 service = new Service1();
+                res = service.mFacturaXML3(null, null, xml);
+            }
+            catch (Exception ex)
+            {
+                res.pResultado = false;
+                res.pDescripcion = ex.Message;
+            }
+
+            return ObtenerDatosResultado(res, xml, "mFacturaXML3");
+        }
+
+        public List<Parameter> AnularDocumento()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Parameter> ObtenerDocumento()
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+
+        //Construcion de listado de datos a retornar por las funciones
+        private List<Parameter> ObtenerDatosResultado(SSO_clsResponseGeneral res, string xml, string function)
+        {
+            List<Parameter> parameters = new List<Parameter>
+            {
+                new Parameter() { ParameterName = "XML", Value = xml },
+                new Parameter() { ParameterName = "Function", Value = function },
+                new Parameter() { ParameterName = "Resultado", Value = res.pResultado },
+                new Parameter() { ParameterName = "Respuesta", Value = null },
+                new Parameter() { ParameterName = "Mensaje", Value = res.pDescripcion },
+                new Parameter() { ParameterName = "IDDoc", Value = res.pIdSat },
+                new Parameter() { ParameterName = "Serie", Value = res.pIdSerie },
+                new Parameter() { ParameterName = "DocNo", Value = res.pDocumentoSiguiente },
+                new Parameter() { ParameterName = "CAE", Value = res.pCAE },
+                new Parameter() { ParameterName = "CAEC", Value = res.pCAEC }
+            };
+
+            return parameters;
+        }
+    }
+}
